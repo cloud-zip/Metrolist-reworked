85f22ca6638632a64084c20be689a7758d281f19
CLOUD 🌨️ 📑 <261238067+cloud-zip@users.noreply.github.com>
2026-05-02 12:01:48 +0530

    Add files via upload

---
+++ app/src/main/kotlin/com/metrolist/music/constants/GradientBackgroundKeys.kt
@@ -0,0 +1,9 @@
+/**
+ * Metrolist Project (C) 2026
+ * Licensed under GPL-3.0 | See git history for contributors
+ */
+
+package com.metrolist.music.constants
+
+const val GradientBackgroundEnabledKey = "gradient_background_enabled"
+const val GradientBackgroundOpacityKey = "gradient_background_opacity"
